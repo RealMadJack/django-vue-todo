5440b6e75e47a4568936e8e67f93ab7fc1ae2fc5
Dmitry G <gupalo1108@gmail.com>
2019-04-04 19:35:32 +0300

    index.html template, api url tests

--- todo/board/tests/test_urls.py
+++ todo/board/tests/test_urls.py
@@ -4,7 +4,15 @@ from django.urls import resolve
 
 class BoardUrlsTest(TestCase):
     def setUp(self):
-        self.resolve_index = resolve('/')
+        self.resolve_api_index = resolve('/api/')
+        self.resolve_api_list = resolve('/api/article/')
+        self.resolve_api_obj = resolve('/api/article/1/')
 
-    def test_index(self):
-        self.assertEqual(self.resolve_index.view_name, 'board:index')
+    def test_api_root(self):
+        self.assertEqual(self.resolve_api_index.view_name, 'board:api-root')
+
+    def test_api_article_list(self):
+        self.assertEqual(self.resolve_api_list.view_name, 'board:article-list')
+
+    def test_api_article_obj(self):
+        self.assertEqual(self.resolve_api_obj.view_name, 'board:article-detail')
--- todo/settings.py
+++ todo/settings.py
@@ -56,7 +56,9 @@ ROOT_URLCONF = 'todo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'todo/templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
