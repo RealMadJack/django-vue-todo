2c4b4925011bb60eb153f291bfd308247b595df0
Dmitry G <gupalo1108@gmail.com>
2019-04-04 14:26:34 +0300

    BoardUrlsTest simple index test

--- todo/board/tests/test_urls.py
+++ todo/board/tests/test_urls.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from django.urls import resolve
 
-# Create your tests here.
+
+class BoardUrlsTest(TestCase):
+    def setUp(self):
+        self.resolve_index = resolve('/')
+
+    def test_index(self):
+        self.assertEqual(self.resolve_index.view_name, 'board:index')
